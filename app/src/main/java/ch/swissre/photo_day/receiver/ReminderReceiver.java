21acc499ccb9aa2580a9de49268cba7ef6ff479d
alex_m_s <alex_m_s@yahoo.com>
2023-03-05 23:06:50 +0100

    added PhotoTaker activity to notification

--- app/src/main/java/ch/swissre/photo_day/receiver/ReminderReceiver.java
+++ app/src/main/java/ch/swissre/photo_day/receiver/ReminderReceiver.java
@@ -3,6 +3,7 @@ package ch.swissre.photo_day.receiver;
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,6 +15,7 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import ch.swissre.photo_day.R;
+import ch.swissre.photo_day.activities.PictureTakerActivity;
 
 public class ReminderReceiver extends BroadcastReceiver {
     private final String CHANNEL_ID = "reminder";
@@ -29,11 +31,16 @@ public class ReminderReceiver extends BroadcastReceiver {
                 NotificationManager.IMPORTANCE_HIGH // Channel importance
         );
 
+        Intent taker = new Intent(context, PictureTakerActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0, taker, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channel.getId())
                 .setSmallIcon(R.drawable.icon)
                 .setContentTitle("Take photo!")
                 .setContentText("It's time to take your daily photo")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {

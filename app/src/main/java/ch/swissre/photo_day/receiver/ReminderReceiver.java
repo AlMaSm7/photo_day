6d5865a107c6691d3baf323facd16213b71b9770
alex_m_s <alex_m_s@yahoo.com>
2023-03-05 22:48:02 +0100

    optimized imports

--- app/src/main/java/ch/swissre/photo_day/receiver/ReminderReceiver.java
+++ app/src/main/java/ch/swissre/photo_day/receiver/ReminderReceiver.java
@@ -7,9 +7,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 
-import androidx.annotation.RequiresApi;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;

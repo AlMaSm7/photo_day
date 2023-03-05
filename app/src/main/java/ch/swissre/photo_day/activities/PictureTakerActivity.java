75b74ff3512d2c0cc5f2651e6c9806a2fbfaec0f
alex_m_s <alex_m_s@yahoo.com>
2023-03-05 15:35:47 +0100

    added camera picture taking feature

--- app/src/main/java/ch/swissre/photo_day/activities/PictureTakerActivity.java
+++ app/src/main/java/ch/swissre/photo_day/activities/PictureTakerActivity.java
@@ -1,57 +1,139 @@
 package ch.swissre.photo_day.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
-import android.content.Intent;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 import ch.swissre.photo_day.R;
 
-public class CameraActivity extends AppCompatActivity {
+public class PictureTakerActivity extends AppCompatActivity {
 
-    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
-    private static final int CAMERA_REQUEST_CODE = 10;
+    private PreviewView previewView;
+    private ImageCapture imageCapture;
 
-    private Button takePhoto;
-    private ImageView picture;
+    private static final int REQUEST_CODE_PERMISSIONS = 1001;
+    private static final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+
+    private Button takePicture;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        // add elements
+        previewView = findViewById(R.id.previewView);
+        takePicture = findViewById(R.id.taker);
 
-        //Set all elements
-        takePhoto = findViewById(R.id.taker);
-        picture = findViewById(R.id.pic);
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            System.out.println("granted");
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    REQUIRED_PERMISSIONS,
+                    REQUEST_CODE_PERMISSIONS
+            );
+        }
 
-        takePhoto.setOnClickListener(view -> {
+        // Wait for the camera provider to be available
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCamera(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e("CameraX", "Error binding preview", e);
+            }
+        }, getExecutor());
 
+        takePicture.setOnClickListener(view -> {
+            takePicture();
         });
     }
 
-    private boolean hasCameraPermission() {
-        return ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED;
+    private void startCamera(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        //Select camera to use
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                .build();
+
+        // Set preview
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        //Take photo
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(
-                this,
-                CAMERA_PERMISSION,
-                CAMERA_REQUEST_CODE
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void takePicture() {
+        long timestamp = System.currentTimeMillis();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(
+                        getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                ).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        System.out.println("Sucesss");
+                        Toast.makeText(PictureTakerActivity.this, "Photo has been saved successfully.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(PictureTakerActivity.this, "Error saving photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                        System.out.println(exception.getMessage());
+                    }
+                }
         );
     }
 
-    private void enableCamera() {
-        Intent intent = new Intent(this, CameraActivity.class);
-        startActivity(intent);
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
     }
 }
